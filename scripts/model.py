d850ec2ef92b973bc6fbf137095fb9f8e70f0413
43741 <hezining@sjtu.edu.cn>
2026-04-17 15:18:35 +0800

    feat: finish building up model

---
+++ scripts/model.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SimpleFaceCNN(nn.Module):
+    def __init__(self, num_classes=4):
+        super(SimpleFaceCNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3,padding=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(64 * 16 * 16, 256)
+
+        self.dropout = nn.Dropout(p=0.5)
+
+        self.fc2 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
+
+        x = x.view(-1, 64 * 16 * 16)
+
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+        return x
+    
+# if __name__ == '__main__':
+#     model = SimpleFaceCNN(num_classes=4)
+#     input = torch.randn(1, 3, 128, 128)
+#     output = model(input)
